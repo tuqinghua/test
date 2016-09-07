cfae739507a879ec063289481eb70cbfb5c5e2a2
tuqinghua <793136390@qq.com>
2016-09-07 11:06:22 +0800

    test

---
+++ a.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="zh-cn">
+<head>
+<title>sss</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<meta name="format-detection" content="telephone=no" />
+<link rel="stylesheet" type="text/css" href="./mobile.css" />
+</head>
+<body>
+
+<div class="content">
+<div class="module">
+<div class="module-hd"><h2 class="fleft">账号信息</h2><a href="/user/logout/" class="fright">退出登录</a></div>
+<div class="accinfo mt10">
+<div class="clearfix">
+<a href="javascript:void(0);" class="fleft username">三中心-涂庆华（2640507）</a>
+<a href="/pay/" class="fright orange">立即充值</a>
+</div>
+</div>
+<div class="accgrade">
+<p>余额：<em class="red">0</em>&nbsp;悠空币&nbsp;<i class="pt-icon lv_0">普</i></p>
+<p>其他：<em class="red">0</em>&nbsp;短信币，&nbsp;<em class="red">0</em>&nbsp;赠送币</p>
+</div>
+</div>
+<div class="module mt10">
+<div class="module-hd"><h2>积分相关</h2></div>
+<div class="accgrade mt10">
+<p style="height:30px;">
+<span class="item"><em class="red">100</em>&nbsp;积分</span>
+<span class="vline">|</span>
+<span class="item"><em class="red">0</em>&nbsp;本正在追</span>
+</p>
+</div>
+</div>
+<div class="module mt10">
+<ul class="linkslist umenu">
+<li><a href="/user/bindoauth/" class="red">绑定其他账号登录</a></li>
+<li><a href="/user/editaccount/" class="red">设置登录账号</a></li>
+<li><a href="/user/logpay/">充值记录</a></li>
+<li><a href="/user/logorder/">订阅记录</a></li>
+<li><a href="/user/autobuy/">自动订阅</a></li>
+</ul>
+</div>
+</div>
+<div class="gray footer">
+<div class="btnav">
+<a href="http://m.yokong.com/">首页</a>
+<span class="vline">|</span>
+<a href="/user/bookcase/">书签</a>
+<span class="vline">|</span>
+<a href="/pay/">充值</a>
+<span class="vline">|</span>
+<a href="/help/">帮助</a>
+<span class="vline">|</span>
+<a href="http://www.yokong.com/app/mobile.html">客户端</a>
+<span class="vline">|</span>
+<a href="/user/logout/">退出登录</a>
+</div>
+<div class="mt10 customsv">
+<h4>客服</h4>
+<p>时间：9:00-22:00（含节假日）</p>
+<p>电话：<a href="tel:025-57955567">025-57955567</a></p>
+<p>QQ：228768606</p>
+</div>
+<div class="mt10 copyright">
+<p>苏ICP备14033810号-2</p>
+<p>Copyright (C) yokong.com. All Rights Reserved.</p>
+</div>
+</div>
+<div style="display:none;"><script type="text/javascript" src="http://res.yokong.com/m/script/tongji.baidu.js"></script></div>
+</div>
+</body>
+</html>
